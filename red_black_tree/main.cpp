33eeffec397f3e2a087a8bad4ebb6f987607445b
Alex Baylon <aunesto15@gmail.com>
2015-11-30 09:26:12 -0500

    Initial Commit

---
+++ red_black_tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  red_black_tree
+//
+//  Created by Alexander Arturo Baylon Ibanez on 30/11/15.
+//  Copyright © 2015 aunesto17. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
